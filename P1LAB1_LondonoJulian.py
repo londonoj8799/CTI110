ad302c4456ab18b92a166743c5225fcb85ab1923
londonoj8799 <londonoj8799@student.faytechcc.edu>
2024-11-12 13:45:16 -0500

    Add files via upload

---
+++ P1LAB1_LondonoJulian.py
@@ -0,0 +1,8 @@
+# Julian Londono
+# 11/12/2024
+# P1LAB1
+# This is a simple program that combines names into variables and combined as output
+
+FirstName = input('Enter your first name:')
+LastName = input('Enter your last name:')
+print('Hello,', FirstName, LastName, '!', 'Welcome to CTI-110')
